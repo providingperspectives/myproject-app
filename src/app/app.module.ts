5ff624cb97c1550a25c1e092001cfe1ddda96bbf
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-03-10 10:39:23 +0100

    http-pipes-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 
 
 import { AppComponent } from './app.component';
@@ -34,6 +35,7 @@ import { RecipeService } from './recipes/recipe.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     AppRoutingModule,
     ReactiveFormsModule
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,19 +9,25 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe [ ] = [new Recipe
-    ('The Traditional Way', 'Change your Entire Concept',
-    'https://i0.wp.com/besthomediet.com/wp-content/uploads/2021/09/Rivers-Native-Soup.jpg',
-    [ new Ingredient('Meat',1 ),
-      new Ingredient('Draw-Soup',6)]),
+//  private recipes: Recipe [ ] = [new Recipe
+ //   ('The Traditional Way', 'Change your Entire Concept',
+ //   'https://i0.wp.com/besthomediet.com/wp-content/uploads/2021/09/Rivers-Native-Soup.jpg',
+ //   [ new Ingredient('Meat',1 ),
+ //     new Ingredient('Draw-Soup',6)]),
 
-  new Recipe
-  ('Wecome Home', 'Home is Home',
-  'https://upload.wikimedia.org/wikipedia/commons/6/6a/Okro_soup_with_shrimps%2Cdried_fish%2Ccow_leg_and_tail_with_meat.jpg',
-  [new Ingredient('Native-Soup',2),
-  new Ingredient('Egusi-Soup', 4)])];
+ // new Recipe
+ // ('Wecome Home', 'Home is Home',
+ // 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Okro_soup_with_shrimps%2Cdried_fish%2Ccow_leg_and_tail_with_meat.jpg',
+ // [new Ingredient('Native-Soup',2),
+//  new Ingredient('Egusi-Soup', 4)])];
 
-  constructor(private slService: ShoppingListService){}
+private recipes: Recipe[] = [];
+constructor(private slService: ShoppingListService){}
+
+ setRecipes(recipes:Recipe[]){
+   this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+ }
 
 getRecipes() {
   return this.recipes.slice();
